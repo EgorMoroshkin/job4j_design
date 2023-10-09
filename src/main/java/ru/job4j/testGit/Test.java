7096124605573d0b6f779af349bd0c0a828f9465
EgorMoroshkin <Westwest38!>
2023-10-09 20:12:31 +0300

    Тест PR

--- src/main/java/ru/job4j/testGit/Test.java
+++ src/main/java/ru/job4j/testGit/Test.java
@@ -2,6 +2,6 @@ package ru.job4j.testGit;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Привет!");
+        System.out.println("Привет2!");
     }
 }
