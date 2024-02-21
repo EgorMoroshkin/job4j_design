6837d43e1fb7097bd1c28af9f58fbe79580657e3
EgorMoroshkin <Westwest38!>
2024-02-21 23:15:49 +0300

    6. Правила Checkstyle [#504981]

---
+++ src/main/java/ru/job4j/сheckstyle/Broken.java
@@ -0,0 +1,24 @@
+package ru.job4j.сheckstyle;
+
+public class Broken {
+    public static final String NEW_VALUE = "";
+    private int sizeOfEmpty = 10;
+    public String surname;
+    String name;
+
+    void echo() {
+    }
+
+    void media(Object obj) {
+        if (obj != null) {
+            System.out.println(obj);
+        }
+    }
+
+    void method(int a, int b, int c, int d, int e, int f, int g) {
+
+    }
+
+    Broken() {
+    }
+}
