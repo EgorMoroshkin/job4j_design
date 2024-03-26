71c7591bddfaba08ca8a93c07dc80b63562b0616
EgorMoroshkin <Westwest38!>
2024-03-26 21:58:31 +0300

    9.0. Файлы и директории. Класс File [#252491]

---
+++ src/main/java/ru/job4j/io/Dir.java
@@ -0,0 +1,22 @@
+package ru.job4j.io;
+
+
+import java.io.File;
+
+public class Dir {
+    public static void main(String[] args) {
+        File file = new File("c:\\projects");
+        if (!file.exists()) {
+            throw new IllegalArgumentException(String.format("Not exist %s", file.getAbsoluteFile()));
+        }
+        if (!file.isDirectory()) {
+            throw new IllegalArgumentException(String.format("Not directory %s", file.getAbsoluteFile()));
+        }
+        System.out.println(String.format("size : %s", file.getTotalSpace()));
+        for (File subfile : file.listFiles()) {
+            System.out.print(String.format("name: %s", subfile.getName()));
+            System.out.print(String.format(" length: %s", subfile.length()));
+            System.out.println();
+        }
+    }
+}
